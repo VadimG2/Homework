3df61e635098236586ca5b7217004a156d00afce
VadimM <vad.gvozdew@yandex.ru>
2024-04-08 20:09:22 +0300

    get colnames and values for one hot

--- Df_to_one_hot.py
+++ Df_to_one_hot.py
@@ -5,3 +5,8 @@ lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
 data.head()
+
+values = data.whoAmI #записываем значения начального DataFrame
+import numpy as np
+classnames, indices = np.unique(values, return_inverse=True) #получаем названия уникальных переменных и их индексы в начальном фрейме
+classnames = list(classnames)
